f4edde2f5ab6f4cdc066eec53258b31998429eb2
J Rose <josh@upros3.com>
2018-02-28 21:52:33 +0000

    exercise 7 finished

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store_id, presence: true
+end
+
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+end
+
+print "TYPE A NAME BRUH: "
+@newer_store_name = gets.chomp
+
+@newer_store = Store.create({name: @newer_store_name})
+
+puts @newer_store.errors.full_messages
