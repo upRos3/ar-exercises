fec6ea3b795b406d6d82bd6eca69893062af83a0
J Rose <josh@upros3.com>
2018-02-28 16:45:57 +0000

    finished exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,29 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+
+burnaby = Store.new(name: 'Burnaby',
+                    annual_revenue: 300_000,
+                    mens_apparel: true,
+                    womens_apparel: true)
+
+burnaby.save
+
+richmond = Store.new(name: 'Richmond',
+                     annual_revenue: 1_260_000,
+                     mens_apparel: false,
+                     womens_apparel: true)
+
+richmond.save
+
+gastown = Store.new(name: 'Gastown',
+                    annual_revenue: 190_000,
+                    mens_apparel: true,
+                    womens_apparel: false)
+
+gastown.save
+
+puts Store.count
